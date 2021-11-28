18306eb11c4d7287c609ebfe10d740b09ddb4d3a
Shahzod Ruziev <shhayitboyevich@gmail.com>
2021-11-28 15:43:25 +0500

    load project

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import dateFilter from '@/filter/dateFilter'
+import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
-
+Vue.filter('date', dateFilter)
 new Vue({
   router,
   store,
